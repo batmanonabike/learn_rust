6afa9443b617c7cc73cd4089e1274ae854e0330d
Martyn C Brown <mart7n@googlemail.com>
2021-04-22 13:54:43 +0100

    using stderr

--- projects/20_io/src/main.rs
+++ projects/20_io/src/main.rs
@@ -16,12 +16,12 @@ fn main() {
     // pass in.
     // Closures are described later.
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem with args: {}", err);
+        eprintln!("Problem with args: {}", err);
         process::exit(1);        
     });
 
     if let Err(e) = io::run(config) {
-        println!("Applicate error: {}", e);
+        eprintln!("Applicate error: {}", e);
         process::exit(1);
     }
 }
